e0b3aafd2448e82c1bc7d428e6dc085126598d0b
D0cWhats0n <johannes.maerkle@gmail.com>
2017-02-03 22:33:18 +0100

    removed subplots

--- ModelLearningCurve.py
+++ ModelLearningCurve.py
@@ -3,17 +3,16 @@
 Created on Fri Feb 03 22:07:18 2017
 
 @author: Johannes Maerkle
-notes: Adapted regression learning curve for decision trees from udacitiy Boston Housing prices project
+notes: Adapted regression learning curve from max_depth for decision trees from udacitiy Boston Housing prices project
 """
 
 import matplotlib.pyplot as pl
 import numpy as np
 import sklearn.learning_curve as curves
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.cross_validation import ShuffleSplit
 
 def ModelLearningCurveClassification(clf, X, y, fold_number=5):
-    """ Calculates the performance of a classification model with varying sizes of training data.
+    """ Calculates the performance of a classification model clf with varying sizes of training data.
         The learning and testing scores for the model are then plotted. """
     
     # Create fold_number cross-validation sets for training and testing
@@ -25,7 +24,7 @@ def ModelLearningCurveClassification(clf, X, y, fold_number=5):
     # Create the figure window
     fig = pl.figure(figsize=(10,7))
 
-        # Calculate the training and testing scores
+    # Calculate the training and testing scores for classifier clf
     sizes, train_scores, test_scores = curves.learning_curve(clf, X, y, \
         cv = cv, train_sizes = train_sizes, scoring = 'accuracy')
         
@@ -36,23 +35,22 @@ def ModelLearningCurveClassification(clf, X, y, fold_number=5):
     test_mean = np.mean(test_scores, axis = 1)
 
     # Subplot the learning curve 
-    ax = fig.add_subplot(2, 2, k+1)
-    ax.plot(sizes, train_mean, 'o-', color = 'r', label = 'Training Score')
-    ax.plot(sizes, test_mean, 'o-', color = 'g', label = 'Testing Score')
-    ax.fill_between(sizes, train_mean - train_std, \
+
+    fig.plot(sizes, train_mean, 'o-', color = 'r', label = 'Training Score')
+    fig.plot(sizes, test_mean, 'o-', color = 'g', label = 'Testing Score')
+    fig.fill_between(sizes, train_mean - train_std, \
         train_mean + train_std, alpha = 0.15, color = 'r')
-    ax.fill_between(sizes, test_mean - test_std, \
+    fig.fill_between(sizes, test_mean - test_std, \
         test_mean + test_std, alpha = 0.15, color = 'g')
         
     # Labels
-    ax.set_title('max_depth = %s'%(depth))
-    ax.set_xlabel('Number of Training Points')
-    ax.set_ylabel('Score')
-    ax.set_xlim([0, X.shape[0]*0.8])
-    ax.set_ylim([-0.05, 1.05])
+    fig.set_title('Learning Curve for Classifier')
+    fig.set_xlabel('Number of Training Points')
+    fig.set_ylabel('Score')
+    fig.set_xlim([0, X.shape[0]*0.8])
+    fig.set_ylim([-0.05, 1.05])
     
     # Visual aesthetics
-    ax.legend(bbox_to_anchor=(1.05, 2.05), loc='lower left', borderaxespad = 0.)
-    fig.suptitle('Decision Tree Regressor Learning Performances', fontsize = 16, y = 1.03)
+    fig.legend(bbox_to_anchor=(1.05, 2.05), loc='lower left', borderaxespad = 0.)
     fig.tight_layout()
     fig.show()
