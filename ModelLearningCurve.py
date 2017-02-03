2c33eae6f44b91fe25ea554b2c3938a40928088f
D0cWhats0n <johannes.maerkle@gmail.com>
2017-02-03 22:23:41 +0100

    removed loop over max-depth

--- ModelLearningCurve.py
+++ ModelLearningCurve.py
@@ -3,20 +3,21 @@
 Created on Fri Feb 03 22:07:18 2017
 
 @author: Johannes Maerkle
+notes: Adapted regression learning curve for decision trees from udacitiy Boston Housing prices project
 """
 
 import matplotlib.pyplot as pl
 import numpy as np
 import sklearn.learning_curve as curves
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.cross_validation import ShuffleSplit, train_test_split
+from sklearn.cross_validation import ShuffleSplit
 
-def ModelLearningCurve(clf, X, y):
-    """ Calculates the performance of several models with varying sizes of training data.
-        The learning and testing scores for each model are then plotted. """
+def ModelLearningCurveClassification(clf, X, y, fold_number=5):
+    """ Calculates the performance of a classification model with varying sizes of training data.
+        The learning and testing scores for the model are then plotted. """
     
-    # Create 10 cross-validation sets for training and testing
-    cv = ShuffleSplit(X.shape[0], n_iter = 10, test_size = 0.2, random_state = 0)
+    # Create fold_number cross-validation sets for training and testing
+    cv = ShuffleSplit(X.shape[0], n_iter = fold_number, test_size = 0.2, random_state = 0)
 
     # Generate the training set sizes increasing by 50
     train_sizes = np.rint(np.linspace(1, X.shape[0]*0.8 - 1, 9)).astype(int)
@@ -24,37 +25,31 @@ def ModelLearningCurve(clf, X, y):
     # Create the figure window
     fig = pl.figure(figsize=(10,7))
 
-    # Create three different models based on max_depth
-    for k, depth in enumerate([1,3,6,10]):
-        
-        # Create a Decision tree regressor at max_depth = depth
-        regressor = DecisionTreeRegressor(max_depth = depth)
-
         # Calculate the training and testing scores
-        sizes, train_scores, test_scores = curves.learning_curve(regressor, X, y, \
-            cv = cv, train_sizes = train_sizes, scoring = 'r2')
+    sizes, train_scores, test_scores = curves.learning_curve(clf, X, y, \
+        cv = cv, train_sizes = train_sizes, scoring = 'accuracy')
         
         # Find the mean and standard deviation for smoothing
-        train_std = np.std(train_scores, axis = 1)
-        train_mean = np.mean(train_scores, axis = 1)
-        test_std = np.std(test_scores, axis = 1)
-        test_mean = np.mean(test_scores, axis = 1)
+    train_std = np.std(train_scores, axis = 1)
+    train_mean = np.mean(train_scores, axis = 1)
+    test_std = np.std(test_scores, axis = 1)
+    test_mean = np.mean(test_scores, axis = 1)
 
-        # Subplot the learning curve 
-        ax = fig.add_subplot(2, 2, k+1)
-        ax.plot(sizes, train_mean, 'o-', color = 'r', label = 'Training Score')
-        ax.plot(sizes, test_mean, 'o-', color = 'g', label = 'Testing Score')
-        ax.fill_between(sizes, train_mean - train_std, \
-            train_mean + train_std, alpha = 0.15, color = 'r')
-        ax.fill_between(sizes, test_mean - test_std, \
-            test_mean + test_std, alpha = 0.15, color = 'g')
+    # Subplot the learning curve 
+    ax = fig.add_subplot(2, 2, k+1)
+    ax.plot(sizes, train_mean, 'o-', color = 'r', label = 'Training Score')
+    ax.plot(sizes, test_mean, 'o-', color = 'g', label = 'Testing Score')
+    ax.fill_between(sizes, train_mean - train_std, \
+        train_mean + train_std, alpha = 0.15, color = 'r')
+    ax.fill_between(sizes, test_mean - test_std, \
+        test_mean + test_std, alpha = 0.15, color = 'g')
         
-        # Labels
-        ax.set_title('max_depth = %s'%(depth))
-        ax.set_xlabel('Number of Training Points')
-        ax.set_ylabel('Score')
-        ax.set_xlim([0, X.shape[0]*0.8])
-        ax.set_ylim([-0.05, 1.05])
+    # Labels
+    ax.set_title('max_depth = %s'%(depth))
+    ax.set_xlabel('Number of Training Points')
+    ax.set_ylabel('Score')
+    ax.set_xlim([0, X.shape[0]*0.8])
+    ax.set_ylim([-0.05, 1.05])
     
     # Visual aesthetics
     ax.legend(bbox_to_anchor=(1.05, 2.05), loc='lower left', borderaxespad = 0.)
